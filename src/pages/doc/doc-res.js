8c6602555f607e2cd8a1cd6a7213b88413582426
dongmingzhixiu <1427953302@qq.com>
2021-04-25 18:13:15 +0800

    append

---
+++ src/pages/doc/doc-res.js
@@ -0,0 +1,53 @@
+export default {
+  layout: [{
+      prop: 'skin',
+      label: '皮肤',
+      type: 'select',
+      options: [{
+        label: '深色',
+        value: 'dark'
+      }, {
+        label: '浅色',
+        value: 'light'
+      }],
+      width:'160px'
+    }, {
+      prop: 'showOutline',
+      label: '是否显示大纲',
+      type: 'select',
+      options: [{
+        label: '是',
+        value: true
+      }, {
+        label: '否',
+        value: false
+      }],
+      width:'200px'
+    },
+    {
+      prop: 'aligns',
+      label: '大纲位置',
+     type: 'select',
+     options: [{
+       label: '居左',
+       value: 'left'
+     }, {
+       label: '居中',
+       value: 'center'
+     },
+     {
+       label: '居右',
+       value: 'right'
+     }],
+     width:'160px'
+    },
+    {
+      prop: 'docWidth',
+      label: '文档内容宽度',
+      placeholder:'100%或100px',
+      msg:'格式错误！',
+      type: 'text',
+      regex:/^[1-6][0-9]{2}px$|[0-9]{3}[%]/,
+    },
+  ]
+}
--- src/pages/doc/doc.vue
+++ src/pages/doc/doc.vue
@@ -1,40 +1,38 @@
 <template>
   <!-- 加载 -->
-  <div class="box-b p10 h loading">
+  <div class="box-b p10 h ld-doc">
     <el-card class="doc w h">
       <ld-doc :doc="doc" class="w h">
-        <template v-slot:loadingTrue="e">
-          <el-card>
-            <ld-page-loading :loading="loading" style="height: 200px;">页面内容</ld-page-loading>
-          </el-card>
+
+        <template v-slot:first="e">
+          <ld-doc :doc="doc1" :show-outline="false" class="w h bor-d">
+            <template v-slot:foot="e">
+              <foot></foot>
+            </template>
+          </ld-doc>
         </template>
-        <div class="m10"></div>
-        <template v-slot:loadingTrueAndFalse="e">
-          <el-button class="m-b2" size="mini" @click="loading2=!loading2">{{loading2?'停止加载':'开始加载'}}</el-button>
-          <el-card>
-            <ld-page-loading :loading="loading2" style="height: 200px;">
-              这里显示的是ld-page-loading插槽中的内容
-            </ld-page-loading>
-          </el-card>
-        </template>
-        <div class="m10"></div>
-        <template v-slot:customLoadingAnimal="e">
-          <el-button class="m-b2" size="mini" @click="loading3=!loading3">{{loading3?'停止加载':'开始加载'}}</el-button>
-          <el-card class="p0">
-            <ld-page-loading :loading="loading3" style="height: 300px;">
-              <template v-if="loading3" v-slot:loading>
-                <div class="w h f-c a-i-c b-i">
-                  <div>
-                    <img src="../../../static/loading.gif" class="w-200 h-160 r10">
-                    <div class="fs36">LOADING....</div>
-                  </div>
+
+
+        <template v-slot:secound="e">
+
+          <ld-forms class="m-t10 m-b10" :form="forms" :layout="formLayout" :is-row="true">
+            <template v-slot:buttons="e">
+              <div style="position: relative;">
+                <el-button type="primary m-l10" @click="copy('customCode')">复制代码</el-button>
+                <div id="customCode" class="o0" style="position: absolute;">
+                  {{forms}}
                 </div>
-              </template>
-              这里显示的是ld-page-loading插槽中的内容
-            </ld-page-loading>
-          </el-card>
+              </div>
+            </template>
+          </ld-forms>
+
+          <ld-doc :doc="doc2" :aligns="forms['aligns']" :skin="forms['skin']" :show-outline="forms['showOutline']" class="w bor-d"
+            style="height: 1000px;">
+            <template v-slot:foot="e">
+              <foot></foot>
+            </template>
+          </ld-doc>
         </template>
-        <div class="m10"></div>
 
         <template v-slot:foot="e">
           <foot></foot>
@@ -45,25 +43,137 @@
 </template>
 
 <script>
+  import res from './doc-res.js';
   export default {
-    name: 'loading',
+    name: 'doc',
     data() {
       return {
-        loading: true,
-        loading2: true,
-        loading3: true,
+        forms: {
+          showOutline: true,
+          skin: 'dark',
+          aligns:'left'
+        },
+        formLayout: res.layout,
+
+        doc1: {
+          h1: 'ld-doc组件是一个轻量级的文档解释组件',
+          h2: 'TA的使用场景通常使用在官方文档中，需要结合示例代码展示，必要时可与需要提供可交互的示例动态结合！',
+          h3: '这样的使用，可以大量复用，可以结合实例动态展示整个要说明程序或组件的各种特性！',
+          slot: 'foot',
+        },
+        doc2: [{
+            title: 'title 标题',
+            tip: '注意：皮肤深色和浅色只对大纲起作用！',
+            h1: '一.常用文档标签',
+          },
+          {
+            h1: 'h1',
+            h2: 'h2',
+            h3: 'h3',
+            tip_d: '`tip_d`标签',
+            tip_i: '`tip_i`标签',
+            tip_w: '`tip_w`标签',
+            tip_s: '`tip_s`标签',
+            tip_p: '`tip_p`标签,也可简写成`tip`',
+            p: 'p标签'
+          },
+          {
+            h1: '二.代码标签',
+            p: '`csharp`, `html`, `css`, `javascript`, `php`, `dart`, `bash`, `shell`, `sql`, `vue`',
+            tip_d: '目前这些标签会在文档中被解析成代码，如需将其他标签也解析成代码，请使用`codeLanguages`进行设置，(但样式方面需要自己去支持！)。当前值为`codeLanguages=["csharp", "html", "css", "javascript", "php", "dart", "bash", "shell", "sql", "vue"]`',
+          }, {
+            csharp: `
+              public void GetData(){
+                return null;
+              }`.replace(/^        /gm, ''),
+            html: `<div>这是html代码</div>`.replace(/^        /gm, ''),
+            css: `
+            .c-d{
+              color:var(--dangercolor);
+            }
+            `.replace(/^        /gm, ''),
+            javascript: `
+            console.log('这是一段javascript代码');
+            `.replace(/^        /gm, ''),
+            php: `
+            <?php
+
+            echo "Hello World!" ;
+            echo "Hello PHP!" ;
+
+            ?>
+            `.replace(/^        /gm, ''),
+            bash: ` npm i `,
+            shell: ` npm update `,
+            sql: ` SELECT * FROM USERS`,
+
+          },
+          {
+            h1: '三.以上文档配置代码如下',
+            javascript: ``,
+            slot: 'foot',
+          }
+        ],
 
         doc: [{
-          H1: '轻量级文档结构化解释组件`ld-doc`',
-          tip: '通过参数控制显示内容',
-          tip_d: '注意：如果在使用中需要使用`markdown`来显示内容且markdown不能正确渲染，请使用如下命令安装markdown文档解释插件`vue-meditor`',
-          bash: 'npm i vue-meditor',
-        }, {
-          slot: 'foot'
-        }]
+            title: '`ld-doc`轻量级文档结构化解释组件',
+            h1: '一.准备',
+            tip: '通过参数控制显示内容',
+            tip_d: '注意：如果在使用中需要使用`markdown`来显示内容且markdown不能正确渲染，请使用如下命令安装markdown文档解释插件`vue-meditor`',
+            bash: 'npm i vue-meditor',
+          },
+          {
+            h1: '二.一个简单的文档示例',
+            h2: '示例代码`不显示大纲，包含h1,h2,h3和slot`',
+          }, {
+            html: `
+          <ld-doc :doc="doc1" :show-outline="false" class="w h b-p2">
+            <!-- 自定义插槽 ，插槽名称右 doc变量中的 slot值决定-->
+            <!-- 这里的作用域插槽名称foot是由下方参数的slot:'foot'决定的-->
+            <template v-slot:foot="e">
+              <foot></foot>
+            </template>
+          </ld-doc>
+          `.replace(/^       /gm, ''),
+            javascript: `
+            export default {
+              data(){
+                return {
+                  data1:{
+                    h1:'ld-doc组件是一个轻量级的文档解释组件',
+                    h2:'TA的使用场景通常使用在官方文档中，需要结合示例代码展示，必要时可与需要提供可交互的示例动态结合！',
+                    h3:'这样的使用，可以大量复用，可以结合实例动态展示整个要说明程序或组件的各种特性！',
+                    slot:'foot',
+                  }
+                }
+              }
+            }
+          `.replace(/^       /gm, ''),
+            h2: '效果如下',
+            slot: 'first',
+          },
+          {
+            h1: '三.文档参数`doc`结构说明',
+            h2: '文档结构支持`Array`,`Object`,`String`类型',
+            h3: '如下是一个动态支持各种类型的示例，可手动设置',
+            tip_d: '如果需要支持多个相同标签，那么将TA分割成两个Object,避免相同键的值覆盖！',
+            slot: 'secound',
+          },
+          {
+            slot: 'foot'
+          }
+        ]
       }
     },
-    created() {}
+    created() {
+      this.doc2[this.doc2.length - 1]['javascript'] =
+      JSON.stringify(this.doc2)
+      .replace(/^/,'\n	 ')
+      .replace(/,"/g,',\n\t\t"')
+      .replace(/"[}]/g,'"\n\t}')
+      .replace(/,[{]/g,',\n\t{\n\t\t')
+      ;
+    }
   }
 </script>
 
